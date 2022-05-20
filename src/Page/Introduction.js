739edac02cae26ed11ea21f2116d1db3e264a52d
doyeonyou <doyeonyou@naver.com>
2022-05-20 22:30:50 +0900

    add mediaquery

--- src/Page/Introduction.js
+++ src/Page/Introduction.js
@@ -25,13 +25,13 @@ const Introduction = props => {
       transition={{
         x: { type: 'spring', stiffness: 300, damping: 30 },
         opacity: { duration: 0.2 },
-      }}>
+      }}
+      className='infoWrap'>
       <div className='page introduction'>
-        <Container className='pBoxs'>
           <Row>
             <Col className='sayHello'>
-              <h1 style={{ fontSize: '80px' }}>안녕하세요!</h1>
-              <h4 style={{ fontSize: '30px' }}>
+              <h1>안녕하세요!</h1>
+              <h4>
                 프론트엔드 개발자를 꿈꾸는 권도연입니다.
               </h4>
               <hr />
@@ -90,7 +90,28 @@ const Introduction = props => {
               nulla voluptatum.
             </Col>
           </Row>
-        </Container>
+          <Row>
+            <Col
+              className='pBox'
+              data-aos='flip-left'
+              data-aos-delay='1500'
+              data-aos-duration='3000'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+              rerum commodi excepturi, fugit saepe nemo laudantium eaque,
+              corporis eveniet fuga minima nesciunt ea? Cum quod quam error non
+              nulla voluptatum.
+            </Col>
+            <Col
+              className='pBox'
+              data-aos='flip-left'
+              data-aos-delay='1800'
+              data-aos-duration='3000'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+              rerum commodi excepturi, fugit saepe nemo laudantium eaque,
+              corporis eveniet fuga minima nesciunt ea? Cum quod quam error non
+              nulla voluptatum.
+            </Col>
+          </Row>
         <Arrow next='stack' />
         <Sidebar introduction={window.location.pathname === '/intro' ? 'true' : 'false'}/>
       </div>
--- src/Page/skillSet/Components/Exit.js
+++ src/Page/skillSet/Components/Exit.js
@@ -6,7 +6,7 @@ import {FiX} from 'react-icons/fi'
 const Exit = () => {
   return (
     <a href='/stack' className='exitIcon'>
-      <FiX/>
+      <FiX className='ico'/>
     </a>
   );
 };
