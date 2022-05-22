46ee6457e809d2d4ed72897c094c5481109d7cfa
doyeonyou <doyeonyou@naver.com>
2022-05-22 13:21:45 +0900

    add metatags

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.scss';
 import { useEffect, useState } from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
-import MetaTags from './Components/MetaTags';
 import Main from './Page/Main';
 import Introduction from './Page/Introduction';
 import Stack from './Page/Stack';
@@ -43,7 +42,6 @@ function App() {
   return (
     <div className='App'>
       <AnimatePresence>
-        <MetaTags/>
         <Routes>
           <Route path='/' element={<Main />} />
           <Route
--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <HelmetProvider>
         <App />
-      </HelmetProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
